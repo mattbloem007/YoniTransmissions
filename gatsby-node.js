ab1fd4f2f3dc5a44922ecdc8cc6333885bcf8d64
Matthew Gabriel Jason Bloemetje <mattb007@gmail.com>
2019-12-10 16:25:16 +0200

    removed node

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,86 +1,86 @@
-const { createFilePath, createRemoteFileNode } = require(`gatsby-source-filesystem`);
-const path = require(`path`);
-
-exports.createPages = ({ graphql, actions }) => {
-    const { createPage } = actions;
-    return graphql(`
-        {
-            wpgraphql {
-            posts {
-              edges {
-                node {
-                  id
-                  slug
-                  date
-                  featuredImage{
-                    sourceUrl
-                  }
-                  categories {
-                    edges {
-                      node {
-                        name
-                      }
-                    }
-                  }
-                }
-              }
-            }
-
-            pages {
-              edges {
-                node {
-                  id
-                  slug
-                }
-              }
-            }
-
-            }
-        }
-
-
-
-
-    `).then(result => {
-      console.log(result.data)
-        const blogPosts = result.data.wpgraphql.posts.edges;
-        const allPages = result.data.wpgraphql.pages.edges;
-        console.log(blogPosts)
-
-        blogPosts.forEach(({ node }) => {
-          let template = (node.categories.edges[0].node.name).toLowerCase();
-          if (template == "about") {
-            createPage({
-                path: node.slug,
-                component: path.resolve("./src/templates/about.js"),
-                context: {
-                    id: node.id,
-                    slug: node.slug,
-                    featuredImage: node.featuredImage,
-                    id2:  {"eq": "SitePage /" + node.slug},
-                    id3: "SitePage /" + node.slug
-                }
-            });
-          }
-          else if (template == "projects") {
-            createPage({
-                path: node.slug,
-                component: path.resolve("./src/templates/projects.js"),
-                context: {
-                    id: node.id,
-                    slug: node.slug,
-                    featuredImage: node.featuredImage,
-                    id2:  {"eq": "SitePage /" + node.slug},
-                    id3: "SitePage /" + node.slug
-                }
-            });
-          }
-
-        });
-
-    });
-};
-
+// const { createFilePath, createRemoteFileNode } = require(`gatsby-source-filesystem`);
+// const path = require(`path`);
+//
+// exports.createPages = ({ graphql, actions }) => {
+//     const { createPage } = actions;
+//     return graphql(`
+//         {
+//             wpgraphql {
+//             posts {
+//               edges {
+//                 node {
+//                   id
+//                   slug
+//                   date
+//                   featuredImage{
+//                     sourceUrl
+//                   }
+//                   categories {
+//                     edges {
+//                       node {
+//                         name
+//                       }
+//                     }
+//                   }
+//                 }
+//               }
+//             }
+//
+//             pages {
+//               edges {
+//                 node {
+//                   id
+//                   slug
+//                 }
+//               }
+//             }
+//
+//             }
+//         }
+//
+//
+//
+//
+//     `).then(result => {
+//       console.log(result.data)
+//         const blogPosts = result.data.wpgraphql.posts.edges;
+//         const allPages = result.data.wpgraphql.pages.edges;
+//         console.log(blogPosts)
+//
+//         blogPosts.forEach(({ node }) => {
+//           let template = (node.categories.edges[0].node.name).toLowerCase();
+//           if (template == "about") {
+//             createPage({
+//                 path: node.slug,
+//                 component: path.resolve("./src/templates/about.js"),
+//                 context: {
+//                     id: node.id,
+//                     slug: node.slug,
+//                     featuredImage: node.featuredImage,
+//                     id2:  {"eq": "SitePage /" + node.slug},
+//                     id3: "SitePage /" + node.slug
+//                 }
+//             });
+//           }
+//           else if (template == "projects") {
+//             createPage({
+//                 path: node.slug,
+//                 component: path.resolve("./src/templates/projects.js"),
+//                 context: {
+//                     id: node.id,
+//                     slug: node.slug,
+//                     featuredImage: node.featuredImage,
+//                     id2:  {"eq": "SitePage /" + node.slug},
+//                     id3: "SitePage /" + node.slug
+//                 }
+//             });
+//           }
+//
+//         });
+//
+//     });
+// };
+//
 // exports.onCreateNode = async ({ node, getNode, actions, store, cache, createNodeId, _auth, }) => {
 //     const { createNodeField, createNode } = actions;
 //     let fileNode
